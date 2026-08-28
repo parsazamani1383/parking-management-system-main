96ded40c185a8805ae4d94c74333f5855eaea3f8
parsa <parsazamani.uok@gmail.com>
2026-08-28 18:59:53 +0330

    Update session_repo.py

--- src/application/interfaces/session_repo.py
+++ src/application/interfaces/session_repo.py
@@ -37,3 +37,38 @@ class SessionRepository(ABC):
         session: ParkingSession
     ) -> None:
         pass
+
+    @abstractmethod
+    def get_all(
+            self
+    ) -> list[ParkingSession]:
+        pass
+
+    @abstractmethod
+    def get_recent_sessions(
+            self,
+            limit: int = 10
+    ) -> list[dict]:
+        pass
+
+
+    @abstractmethod
+    def search_active_vehicles(
+            self,
+            plate_part: str
+    ) -> list[dict]:
+        pass
+
+    @abstractmethod
+    def get_active_session_info(
+            self,
+            session_id: int
+    ) -> dict | None:
+        pass
+
+    @abstractmethod
+    def get_daily_revenue(
+            self,
+            days: int,
+    ) -> list[dict]:
+        pass
